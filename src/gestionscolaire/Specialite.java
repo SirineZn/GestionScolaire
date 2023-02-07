c0d979810bc04704bf168d953c88308c0bab669a
eyasoussia <eya.soussia@esprit.tn>
2023-02-07 21:56:50 +0100

    voici mes modifications dans les classes

--- src/gestionscolaire/Specialite.java
+++ src/gestionscolaire/Specialite.java
@@ -10,5 +10,31 @@ package gestionscolaire;
  * @author sirine
  */
 public class Specialite {
+    private int Id_spec;
+    private String nom_spec;
+    private String[] tab_matieres;
     
+    public int getIdSpec() {
+      return Id_spec;
+    }
+    
+    public void setIdSpec(int Id_spec) {
+      this.Id_spec = Id_spec;
+    }
+    
+    public String getNomSpec() {
+      return nom_spec;
+    }
+    
+    public void setNomSpec(String nom_spec) {
+      this.nom_spec = nom_spec;
+    }
+    
+    public String[] getTabMatieres() {
+      return tab_matieres;
+    }
+    
+    public void setTabMatieres(String[] tab_matieres) {
+      this.tab_matieres = tab_matieres;
+    }
 }
