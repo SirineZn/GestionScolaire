012745e9dc13fca432489565a103dc1aa7f5bd85
eyasoussia <eya.soussia@esprit.tn>
2023-02-07 22:06:14 +0100

    j'ai commité le travail

--- src/gestionscolaire/Matiere.java
+++ src/gestionscolaire/Matiere.java
@@ -9,6 +9,41 @@ package gestionscolaire;
  *
  * @author sirine
  */
-public class Université {
-    
+public class Matiere {
+    private int Id_mat;
+  private String Nom_mat;
+  private float note;
+  private float coefficient;
+  
+  public int getIdMat() {
+    return Id_mat;
+  }
+  
+  public void setIdMat(int Id_mat) {
+    this.Id_mat = Id_mat;
+  }
+  
+  public String getNomMat() {
+    return Nom_mat;
+  }
+  
+  public void setNomMat(String Nom_mat) {
+    this.Nom_mat = Nom_mat;
+  }
+  
+  public float getNote() {
+    return note;
+  }
+  
+  public void setNote(float note) {
+    this.note = note;
+  }
+  
+  public float getCoefficient() {
+    return coefficient;
+  }
+  
+  public void setCoefficient(float coefficient) {
+    this.coefficient = coefficient;
+  }
 }
